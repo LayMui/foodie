ad579acd73f115e3cb8f62a5bbb375e1d3b05538
LayMui <mui_ume@hotmail.com>
2021-08-04 21:22:06 +0800

    Initial Commit

---
+++ features/step-definitions/steps.ts
@@ -0,0 +1,23 @@
+import { Given, When, Then } from '@cucumber/cucumber';
+
+import HomePage from '../pageobjects/home.page';
+
+
+const pages = {
+    food: HomePage
+}
+
+Given(/^(?:.*) is at the (\w+) app$/, async (page) => {
+    await pages[page].open()
+});
+
+
+When(/^he wants to search for restaurant name (\w+)$/, async (name) => {
+    await HomePage.searchFor(name);
+});
+
+
+Then(/^he is able to see the result of his search$/, async () => {
+   
+});
+
